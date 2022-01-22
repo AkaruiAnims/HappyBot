b4af08a3fe099c2f1d336b5d6a2320c27b01bedd
Jerpharney <60879671+AkaruiAnims@users.noreply.github.com>
2022-01-22 00:12:29 +0000

    working on functionality

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 import discord
 import os
 import requests
-import datetime
 import asyncio
-
+from pytz import timezone, all_timezones
+from datetime import datetime
 from replit import db
 from active import keep_alive
 from discord.ext import commands
 
 token = os.getenv("token")
 bot = commands.Bot(command_prefix="happy ", description="Discord Vistual Assistant")
+prefix = "happy"
+again = ""
+prevAuthor = ""
+msgStage = 0
+free = True
 
 @bot.event
 async def  on_ready():
     print("Active")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('**pong**')
+@bot.event
+async def on_message(message):
+  if message.author == bot.user:
+    return
+
+  global again
+  if message.content.startswith('happy again') or message.content.startswith('again happy') and again != "":
+    message.content = again
+
+  if message.content.startswith('what time is it happy') or message.content.startswith('happy what time is it') or message.content.startswith('what time is it'):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    await message.channel.send(f"Right now it's  {current_time}")
+
+  if message.content.startswith('how do i get started happy'):
+    await message.channel.send(f"""To get strated say; 'happy register me'.
+Answer some questions and I'll take care fo the rest :)""")
+  
+  again = message.content
     
 keep_alive()
 bot.run(token)
